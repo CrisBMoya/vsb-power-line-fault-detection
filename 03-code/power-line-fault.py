4b7ef40ab8f6911be45922996dd624c40b7a2e70
Cristobal Balladares <cr.balladares.m@gmail.com>
2021-12-03 12:05:41 -0300

    upd

---
+++ 03-code/power-line-fault.py
@@ -0,0 +1,10 @@
+import pyarrow.parquet as pq
+import pandas as pd
+import numpy as np
+import datatable as dt
+
+for i in range(8711):
+    print(i)
+    col_x = pq.read_pandas("D:/Documentos/raw-data/vsb-power-line-fault-detection/train.parquet", columns=[str(i)).to_pandas()
+    col_x.to_csv(f"D:/Documentos/raw-data/vsb-power-line-fault-detection/train_data/{str(i)}.csv")
+
